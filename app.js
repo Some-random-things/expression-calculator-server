231e28907b39180330273eff010e4dd5c2a47c84
Medvedev <dima4538@mail.ru>
2016-03-22 05:43:27 +0300

    Body parser added in middleware

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
 var algorythmController = require('./controllers/algorithmController.js');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.post('/', algorythmController.calculate);
+app.get('/', algorythmController.calculate);
 
 app.listen(3004, function() {
   console.log('Server is running');
